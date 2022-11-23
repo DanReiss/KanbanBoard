6e45d5533951842836698a84448fe8fbdedab753
Danreiss <danreis.ctt@gmail.com>
2022-11-23 12:44:11 -0300

    finalizado-no-responsive

--- script.js
+++ script.js
@@ -1,79 +1,121 @@
-let tasks = document.querySelectorAll(".task")
-const dropzones = document.querySelectorAll(".dropzone")
-const input = document.querySelector(".task-name")
-let deletes = document.querySelectorAll(".delete")
+let tasks = document.querySelectorAll(".task");
+const dropzones = document.querySelectorAll(".dropzone");
+const input = document.querySelector(".task-name");
+let deletes = document.querySelectorAll(".delete");
 
 
-function createKanban(){
-    if(input.value.length !== 0){
-    let newKanban = document.createElement("div")
-    newKanban.classList.add("task", "bg-info", "rounded", "p-1", "my-2")
-    newKanban.setAttribute("draggable", true)
-    let newStatus = document.createElement("div")
-    newStatus.classList.add("status")
-    newKanban.appendChild(newStatus)
-    let newBox = document.createElement("h5")
-    newBox.innerText = input.value;
-    newKanban.appendChild(newBox)
-    let newDelete = document.createElement("img")
-    newDelete.setAttribute("src", "./assets/x-svgrepo-com.svg")
-    newDelete.classList.add("delete")
-    newKanban.appendChild(newDelete)
-    dropzones[0].appendChild(newKanban)
+window.addEventListener("load", reload);
+window.addEventListener("load", addSavedTasks);
+
+function createKanban(localTask, board){
+    if(input.value.length !== 0 || localTask){
+        let newKanban = document.createElement("div");
+        newKanban.classList.add("task", "bg-info", "rounded", "p-1", "my-2");
+        newKanban.setAttribute("draggable", true);
+        let newStatus = document.createElement("div");
+        newStatus.classList.add("status");
+        newKanban.appendChild(newStatus);
+        let newBox = document.createElement("h5");
+        newBox.innerText = input.value || localTask; // add tasks salvas || taskLocal
+        newKanban.appendChild(newBox);
+        let newDelete = document.createElement("img");
+        newDelete.setAttribute("src", "./assets/x-svgrepo-com.svg");
+        newDelete.classList.add("delete");
+        newKanban.appendChild(newDelete);
+        if(localTask){
+            dropzones[board].appendChild(newKanban);
+        }else{
+            dropzones[0].appendChild(newKanban);
+        }
     reload();
-    }
-    else{
-        alert("Preencha o campo!")
-    }
+    save();
+    }else{
+        alert("Preencha o campo!");
+    };
+};
+
+
+function save(){ 
+    localStorage.clear(); 
+    
+    const titlesTodo = [];
+    const tasksTodo = document.querySelectorAll(".todo .task h5")
+    tasksTodo.forEach((item)=>{
+       titlesTodo.push(item.innerText);
+       localStorage.setItem("todo", JSON.stringify(titlesTodo))
+   });
+
+    const titlesInProgress = [];
+    const tasksInProgress = document.querySelectorAll(".in-progress .task h5")
+    tasksInProgress.forEach((item)=>{
+       titlesInProgress.push(item.innerText);
+       localStorage.setItem("progress", JSON.stringify(titlesInProgress))
+   });
+
+    const titlesDone = [];
+    const tasksDone = document.querySelectorAll(".done .task h5");
+    tasksDone.forEach((item)=>{
+       titlesDone.push(item.innerText) ;
+       localStorage.setItem("done", JSON.stringify(titlesDone));
+   });
+}
+
+function addSavedTasks(){
+    const tasksTodo = JSON.parse(localStorage.getItem("todo"));
+    const tasksInProgress = JSON.parse(localStorage.getItem("progress"));
+    const tasksDone = JSON.parse(localStorage.getItem("done"));
+    tasksTodo ? tasksTodo.forEach(item => createKanban(item, 0)) : false;
+    tasksInProgress ? tasksInProgress.forEach(item => createKanban(item, 1)) : false;
+    tasksDone ? tasksDone.forEach(item => createKanban(item, 2)) : false;
 }
 
 
 function reload(){
-    tasks = document.querySelectorAll(".task")
-    deletes = document.querySelectorAll(".delete") 
+    tasks = document.querySelectorAll(".task");
+    deletes = document.querySelectorAll(".delete");
 
     tasks.forEach(task =>{
         task.addEventListener('dragstart', dragStart);
         task.addEventListener('dragend', dragEnd);
     });
     deletes.forEach(deleteItem =>{
-        deleteItem.addEventListener('click', addDelete)
-    });
-
+        deleteItem.addEventListener('click', addDelete);
+    });  
 };
 
+
 function addDelete(){
-    let parentDelete = this.parentElement
-    parentDelete.parentElement.removeChild(parentDelete)
-}
+    let parentDelete = this.parentElement;
+    parentDelete.parentElement.removeChild(parentDelete);
+    save();
+};
 
 
 function dragStart(){
-    dropzones.forEach(dropzone => dropzone.classList.add("highlight"))
-    this.classList.add('is-dragging')
-}
+    dropzones.forEach(dropzone => dropzone.classList.add("highlight"));
+    this.classList.add('is-dragging');
+};
 
 function dragEnd(){
-    dropzones.forEach(dropzone => dropzone.classList.remove("highlight"))
-    this.classList.remove('is-dragging')
-}
+    dropzones.forEach(dropzone => dropzone.classList.remove("highlight"));
+    this.classList.remove('is-dragging');
+    save();
+};
 
 dropzones.forEach(dropzone =>{
-    dropzone.addEventListener('dragover', dragOver)
-    dropzone.addEventListener('dragleave', dragLeave)
-    dropzone.addEventListener('drop', drop)
-})
+    dropzone.addEventListener('dragover', dragOver);
+    dropzone.addEventListener('dragleave', dragLeave);
+    dropzone.addEventListener('drop', drop);
+});
 
 function dragOver(){
-   this.classList.add("in-over")
+   this.classList.add("in-over");
 
-   const cardDrag = document.querySelector('.is-dragging')
+   const cardDrag = document.querySelector('.is-dragging');
    
-   this.appendChild(cardDrag)
-}
+   this.appendChild(cardDrag);
+};
 
-function dragLeave(){this.classList.remove("in-over")}   
+function dragLeave(){this.classList.remove("in-over")}; 
 
-function drop(){his.classList.remove("in-over")}
-
-reload();
+function drop(){his.classList.remove("in-over")};
